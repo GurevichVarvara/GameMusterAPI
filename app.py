df84cf7fd638ace5fde57fdf0144c1b235a2c0aa
VarvaraGurevich <gurevichvarvara@gmail.com>
2021-07-08 15:58:24 +0300

    Set up db

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__.split('.')[0])
 app.config['DEBUG'] = bool(os.environ.get('DEBUG', False))
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
 
+if __name__ == '__main__':
+    app.run()
