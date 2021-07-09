1e8f1c7ca03dfec95afc70d7c820d6cf6bfe985e
VarvaraGurevich <gurevichvarvara@gmail.com>
2021-07-09 17:23:33 +0300

    Add all relationships to game model

--- models.py
+++ models.py
@@ -1,4 +1,19 @@
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+
 from config import db
+
+game_platforms = db.Table(
+    'gameMuster_game_platforms',
+    db.Column('game_id', db.Integer, db.ForeignKey('gameMuster_game.id'), primary_key=True),
+    db.Column('platform_id', db.Integer, db.ForeignKey('gameMuster_platform.id'), primary_key=True)
+)
+
+game_genres = db.Table(
+    'gameMuster_game_genres',
+    db.Column('game_id', db.Integer, db.ForeignKey('gameMuster_game.id'), primary_key=True),
+    db.Column('genre_id', db.Integer, db.ForeignKey('gameMuster_genre.id'), primary_key=True)
+)
 
 
 class Platform(db.Model):
@@ -15,8 +30,21 @@ class Genre(db.Model):
     name = db.Column(db.String)
 
 
+class Screenshot(db.Model):
+    __tablename__ = 'gameMuster_screenshot'
+
+    id = db.Column(db.Integer, primary_key=True)
+    img_url = db.Column(db.String)
+    game_id = db.Column(db.Integer, ForeignKey('gameMuster_game.id'))
+
+
 class Game(db.Model):
     __tablename__ = 'gameMuster_game'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+    screenshots = relationship('Screenshot')
+    genres = db.relationship('Genre', secondary=game_genres, lazy='subquery',
+                             backref=db.backref('gameMuster_game', lazy=True))
+    platforms = db.relationship('Platform', secondary=game_platforms, lazy='subquery',
+                                backref=db.backref('gameMuster_game', lazy=True))
--- serializers.py
+++ serializers.py
@@ -1,6 +1,7 @@
+from marshmallow import fields
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 
-from models import Platform, Genre, Game
+from models import Platform, Genre, Game, Screenshot
 
 
 class PlatformSerializer(SQLAlchemyAutoSchema):
@@ -15,7 +16,17 @@ class GenreSerializer(SQLAlchemyAutoSchema):
         load_instance = True
 
 
+class ScreenshotSerializer(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Screenshot
+        load_instance = True
+
+
 class GameSerializer(SQLAlchemyAutoSchema):
+    screenshots = fields.Nested(ScreenshotSerializer, many=True)
+    platforms = fields.Nested(PlatformSerializer, many=True)
+    genres = fields.Nested(GenreSerializer, many=True)
+
     class Meta:
         model = Game
         load_instance = True
