f46d6b74e4eb054bec6a748beee1421e748ededd
VarvaraGurevich <gurevichvarvara@gmail.com>
2021-07-14 18:08:51 +0300

    Add single user endpoint

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import request
+from flask import request, abort
 
 from base_resources import BaseResource, BaseSequenceResource
 from config import app, api
@@ -52,6 +52,25 @@ class Users(BaseSequenceResource):
         return result
 
 
+class SingleUser(BaseResource):
+    def __init__(self):
+        super().__init__(User, UserSerializer())
+
+    @staticmethod
+    def _get_user_by_token(token):
+        user_id = User.decode_auth_token(token)
+
+        return User.query.get(user_id)
+
+    def get(self, object_id):
+        result_user = self._get_user_by_token(request.headers['Authorization'])
+
+        if result_user.id != object_id:
+            abort(404)
+
+        return super().get(object_id)
+
+
 api.add_resource(Platforms, '/api/platforms')
 api.add_resource(SinglePlatform, '/api/platforms/<string:object_id>')
 api.add_resource(Genres, '/api/genres')
@@ -59,6 +78,7 @@ api.add_resource(SingleGenre, '/api/genres/<string:object_id>')
 api.add_resource(Games, '/api/games')
 api.add_resource(SingleGame, '/api/games/<string:object_id>')
 api.add_resource(Users, '/api/users')
+api.add_resource(SingleUser, '/api/profile')
 
 
 if __name__ == '__main__':
