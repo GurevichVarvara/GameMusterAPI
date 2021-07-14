36f240945f5d4297e48442ddfcf7005e7509ffb6
VarvaraGurevich <gurevichvarvara@gmail.com>
2021-07-14 10:45:27 +0300

    Add rest api platforms, genres, games endpoints

--- app.py
+++ app.py
@@ -1,30 +1,37 @@
-from config import app
+from flask_restful import Resource
+
+from config import app, api
 from models import Platform, Genre, Game
 from serializers import PlatformSerializer, GenreSerializer, GameSerializer
 
 
-@app.route('/api/platforms')
-def get_platforms():
-    platforms = Platform.query.order_by(Platform.id).all()
-    serializer = PlatformSerializer()
+class Platforms(Resource):
+    def get(self):
+        platforms = Platform.query.order_by(Platform.id).all()
+        serializer = PlatformSerializer()
 
-    return serializer.dumps(platforms, many=True)
-
-
-@app.route('/api/genres')
-def get_genres():
-    genres = Genre.query.order_by(Genre.id).all()
-    serializer = GenreSerializer()
-
-    return serializer.dumps(genres, many=True)
+        return serializer.dumps(platforms, many=True)
 
 
-@app.route('/api/games')
-def get_games():
-    games = Game.query.order_by(Game.id).all()
-    serializer = GameSerializer()
+class Genres(Resource):
+    def get(self):
+        genres = Genre.query.order_by(Genre.id).all()
+        serializer = GenreSerializer()
 
-    return serializer.dumps(games, many=True)
+        return serializer.dumps(genres, many=True)
+
+
+class Games(Resource):
+    def get(self):
+        games = Game.query.order_by(Game.id).all()
+        serializer = GameSerializer()
+
+        return serializer.dumps(games, many=True)
+
+
+api.add_resource(Platforms, '/api/platforms')
+api.add_resource(Genres, '/api/genres')
+api.add_resource(Games, '/api/games')
 
 
 if __name__ == '__main__':
